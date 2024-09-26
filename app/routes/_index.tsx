264d67265e53c076971a7b6fb23ba7539d1e7249
njiah <nsdl1411@gmail.com>
2024-09-26 19:54:01 +0100

    react app

--- app/routes/_index.tsx
+++ app/routes/_index.tsx
@@ -1,11 +1,12 @@
 import type { MetaFunction } from "@remix-run/node";
 import { Link } from "@remix-run/react";
+//import { InferGetStaticPropsType } from "next";
 
 import { useOptionalUser } from "~/utils";
 
 export const meta: MetaFunction = () => [{ title: "Remix Notes" }];
 
-export default function Index() {
+export default function Index(){
   const user = useOptionalUser();
   return (
     <main className="relative min-h-screen bg-white sm:flex sm:items-center sm:justify-center">
@@ -23,7 +24,7 @@ export default function Index() {
             <div className="relative px-4 pb-8 pt-16 sm:px-6 sm:pb-14 sm:pt-24 lg:px-8 lg:pb-20 lg:pt-32">
               <h1 className="text-center text-6xl font-extrabold tracking-tight sm:text-8xl lg:text-9xl">
                 <span className="block uppercase text-yellow-500 drop-shadow-md">
-                  Indie Stack
+                  Ngwe Sandar Linn
                 </span>
               </h1>
               <p className="mx-auto mt-6 max-w-lg text-center text-xl text-white sm:max-w-3xl">
---
+++ next.config.js
@@ -0,0 +1,21 @@
+/** @type {import('next').NextConfig} */
+const nextConfig = {
+    reactStrictMode: true,
+    env: {
+      SANITY_PROJECT_ID: process.env.SANITY_PROJECT_ID,
+      SANITY_DATASET: process.env.SANITY_DATASET,
+      SANITY_API_VERSION: process.env.SANITY_API_VERSION,
+    },
+    images: {
+      remotePatterns: [
+        {
+          protocol: "https",
+          hostname: "cdn.sanity.io",
+          port: "",
+          pathname: "/images/**",
+        },
+      ],
+    },
+  };
+  
+  module.exports = nextConfig;
